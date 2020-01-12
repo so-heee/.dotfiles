51e4b1d0c5cc72ce858834158ef3144f40e589ee
s_mochizuki <mochizuki.sohei.service@gmail.com>
2020-01-12 22:55:34 +0900

    update

---
+++ setup/03_other.md
@@ -0,0 +1,12 @@
+## VSCode
+Setting Syncsによる同期
+
+- VSCodeに拡張機能「Setting Sync」をインストール
+- コマンドパレット - Sync: 高度なオプション - Sync: 設定を開く
+- 設定画面でGistIDとアクセストークンを登録
+- 「Shift + Option + D」で設定をダウンロード
+- 再起動
+
+以下のオプションを有効にしていく
+- vscode-icons
+- Dracula Theme
--- zsh/plugins.zsh
+++ zsh/plugins.zsh
@@ -1,7 +1,10 @@
-# install Prezto.
+#----------------------------------------
+# Setting Prezto
+#----------------------------------------
+# Install Prezto.
 if [[ ! -d ~/.zprezto ]];then
   git clone --recursive https://github.com/sorin-ionescu/prezto.git "${ZDOTDIR:-$HOME}/.zprezto"
-  # backup .zshrc
+  # Backup .zshrc
   if [[ ! -d ~/.zshrc ]];then
     cp .zshrc .zshrc.before
   fi
@@ -16,7 +19,9 @@ if [[ -s "${ZDOTDIR:-$HOME}/.zprezto/init.zsh" ]]; then
   source "${ZDOTDIR:-$HOME}/.zprezto/init.zsh"
 fi
 
-# Setting PowerLevel9k. 
+#----------------------------------------
+# Setting PowerLevel9k
+#----------------------------------------
 POWERLEVEL9K_LEFT_PROMPT_ELEMENTS=(dir vcs status)
 POWERLEVEL9K_RIGHT_PROMPT_ELEMENTS=(date time)
 
@@ -36,16 +41,39 @@ POWERLEVEL9K_RPROMPT_ON_NEWLINE=false
 POWERLEVEL9K_MULTILINE_FIRST_PROMPT_PREFIX=""
 POWERLEVEL9K_MULTILINE_LAST_PROMPT_PREFIX="\u25B8 "
 
-# SettingPeco
-function peco-history-selection() {
-    BUFFER=$(history 1 | sort -k1,1nr | perl -ne 'BEGIN { my @lines = (); } s/^\s*\d+\*?\s*//; $in=$_; if (!(grep {$in eq $_} @lines)) { push(@lines, $in); print $in; }' | peco --query "$LBUFFER")
-    CURSOR=${#BUFFER}
-    zle reset-prompt
-}
-zle -N peco-history-selection
-bindkey '^R' peco-history-selection
+#----------------------------------------
+# Setting Peco
+#----------------------------------------
+# function peco-history-selection() {
+#     BUFFER=$(history 1 | sort -k1,1nr | perl -ne 'BEGIN { my @lines = (); } s/^\s*\d+\*?\s*//; $in=$_; if (!(grep {$in eq $_} @lines)) { push(@lines, $in); print $in; }' | peco --query "$LBUFFER")
+#     CURSOR=${#BUFFER}
+#     zle reset-prompt
+# }
+# zle -N peco-history-selection
+# bindkey '^R' peco-history-selection
 
+#----------------------------------------
+# Setting fzf
+#----------------------------------------
+# Setup fzf
+if [[ ! "$PATH" == */usr/local/opt/fzf/bin* ]]; then
+  export PATH="${PATH:+${PATH}:}/usr/local/opt/fzf/bin"
+fi
+
+# Auto-completion
+[[ $- == *i* ]] && source "/usr/local/opt/fzf/shell/completion.zsh" 2> /dev/null
+
+# Key bindings
+source "/usr/local/opt/fzf/shell/key-bindings.zsh"
+
+# Env
+export FZF_DEFAULT_COMMAND='rg --files --hidden --follow --glob "!.git/*"'
+export FZF_DEFAULT_OPTS="--height 60% --reverse --border"
+export FZF_CTRL_T_OPTS='--preview "bat  --color=always --style=header,grid --line-range :100 {}"'
+
+#----------------------------------------
 # Setting tmux session
+#----------------------------------------
 function tm() { 
        if [ -n "${1}" ]; then
            tmux attach-session -t ${1} || \
