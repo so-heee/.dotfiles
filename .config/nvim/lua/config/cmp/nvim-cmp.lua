4fb46ddadc625a464ecdddef046b0c32297bca72
so-heee <so.heee.mochi@gmail.com>
2022-04-28 18:33:30 +0900

    fix nvim-cmp

--- .config/nvim/lua/config/cmp/nvim-cmp.lua
+++ .config/nvim/lua/config/cmp/nvim-cmp.lua
@@ -22,9 +22,7 @@ cmp.setup {
     ['<C-e>'] = cmp.mapping.close(),
     ['<CR>'] = cmp.mapping.confirm { select = true },
     ['<Tab>'] = cmp.mapping(function(fallback)
-      if cmp.visible() then
-        cmp.select_next_item()
-      elseif luasnip.expandable() then
+      if luasnip.expandable() then
         luasnip.expand()
       elseif luasnip.expand_or_jumpable() then
         luasnip.expand_or_jump()
@@ -36,9 +34,7 @@ cmp.setup {
       's',
     }),
     ['<S-Tab>'] = cmp.mapping(function(fallback)
-      if cmp.visible() then
-        cmp.select_prev_item()
-      elseif luasnip.jumpable(-1) then
+      if luasnip.jumpable(-1) then
         luasnip.jump(-1)
       else
         fallback()
