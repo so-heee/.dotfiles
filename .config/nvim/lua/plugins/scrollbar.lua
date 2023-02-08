270a46068fa9e259209799df25775aae467b0080
so-heee <so.heee.mochi@gmail.com>
2023-02-08 10:31:42 +0900

    fix scrollbar nvim

--- .config/nvim/lua/plugins/scrollbar.lua
+++ .config/nvim/lua/plugins/scrollbar.lua
@@ -1,7 +1,5 @@
 local scrollbar_status_ok, scrollbar = pcall(require, 'scrollbar')
-local search_status_ok, search = pcall(require, 'scrollbar.handlers.search')
-if not search_status_ok and not scrollbar_status_ok then
+if not scrollbar_status_ok then
   return
 end
-search.setup()
 scrollbar.setup()
